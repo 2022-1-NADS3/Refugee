708527eb970c13ed9999a99ee98bbe70d5727015
Degocardoso <degofagundes088@gmail.com>
2022-05-05 22:32:57 -0300

    Inicio do back-end(Em Aula)

---
+++ src/Backend/ServidorRefugee/index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+var hostname = "localhost";
+const json ='[{"nome":"antonio","sobrenome":" Carlos","altura":1.79},{"nome":"dego","sobrenome":" cardoso","altura":1.78}]';
+var objeto = JSON.parse(json);
+
+app.get("/", function(req,res){
+    var nome = req.query.nome;
+    res.send("Olá pessoa!"+objeto[0].nome+objeto[1].sobrenome+objeto[0].altura);
+    console.log("Passei aqui!");
+});
+
+app.get("/teste1", function(req,res){
+    res.send("<h1>Carai cuzão<h1>");
+})
+
+app.listen(port, hostname, () => {
+    console.log("Servidor http//"+hostname+":"+port);
+});
