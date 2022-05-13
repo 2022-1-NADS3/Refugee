5dcad4823dfa9182875291625f527fe1a54c698c
Degocardoso <degofagundes088@gmail.com>
2022-05-12 22:28:46 -0300

    Servidor (aula)

--- src/Backend/ServidorRefugee/index.js
+++ src/Backend/ServidorRefugee/index.js
@@ -1,12 +1,9 @@
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
-var hostname = "localhost";
+//var hostname = "localhost";
 const json ='[{"nome":"antonio","sobrenome":" Carlos","altura":1.79},{"nome":"dego","sobrenome":" cardoso","altura":1.78}]';
 var objeto = JSON.parse(json);
-var bodyParser = require('body-parser')
-
-app.use(bodyParser.json('application/json'));
 
 app.get("/", function(req,res){
     var nome = req.query.nome;
@@ -18,25 +15,10 @@ app.get("/teste1", function(req,res){
     res.send("<h1>Carai cuzão<h1>");
 })
 
-app.get("/user", function(req,res){
-    console.log("Enviei po Get");
-    res.send(JSON.stringify({
-        nome:"Teo",
-        sobrenome:"balão",
-        idade:22,
-        altura:1.75
-    }));
+app.listen(port, () => {
+    console.log("Servidor http//"+port);
 });
 
-app.post("/add", function(req,res){
-    console.log("recebi um dado!")
-    console.log(req.body.nome);
-    console.log(req.body.sobrenome);
-    console.log(req.body.idade);
-    console.log(req.body.altura);
-    res.send("JSON Recebido!")
-});
-
-app.listen(port, hostname, () => {
-    console.log("Servidor http//"+hostname+":"+port);
-});
+app.post("https://pure-ravine-77933.herokuapp.com/cadastro", function(req,res){
+    
+})
